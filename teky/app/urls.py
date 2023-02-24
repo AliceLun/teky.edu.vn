9542c471d17e210e78f8f29282104c0c95c0d0eb
Thủy Nguyễn <nguyenthuyk69.hnue@gmail.com>
2023-02-24 10:20:22 +0700

    Teky

---
+++ teky/app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.login),
+    path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
+    
+
+]
---
+++ teky/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def login(request):
+    context={}
+    return render(request,'app/login.html',context)
+def home(request):
+    context={}
+    return render(request,'app/home.html',context)
