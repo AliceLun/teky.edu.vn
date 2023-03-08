04ff4702553eaaa296508ccb75ad6c78b9387a6d
Thủy Nguyễn <nguyenthuyk69.hnue@gmail.com>
2023-03-08 15:14:20 +0700

    change Person model

--- teky/app/models.py
+++ teky/app/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
 class Person(models.Model):
-    SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+  
+    username = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
 
         
